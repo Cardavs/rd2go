6eb68fe519702c74d2812357d9fc9d01d237187f
Cardavs <josuecardav@gmail.com>
2023-05-13 21:39:44 -0600

    Header, Footer y Parte del contenido

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {BsLinkedin, BsGithub, BsYoutube, BsInstagram, BsLightbulb} from "react-icons/bs";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return( 
+    <>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className='mb-0 text-white'>Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input 
+                type="text" 
+                className="form-control py-1" 
+                placeholder="Ingresa tu dirección de correo" 
+                aria-label="Ingresa tu dirección de correo" 
+                aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">Suscribirse</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className='text-white mb-4'>Contactanos</h4>
+              <div className='text-white fs-6'>
+                <address>Hno : 277 Near Vill chopal, <br /> Sonipat, Haryana <br /> PinCode: 131103</address>
+                <a href="tel:+23 23986752" className="mt-3 d-block mb-1 text-white">+23 23986753
+                </a>
+                <a href="mailto:rd2go@gmail.com" className="mt-2 d-block mb-0 text-white">rd2go@gmail.com
+                </a>
+                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                  <a className='text-white' href="">
+                    <BsLinkedin className='fs-4'/>
+                  </a>
+                  <a className='text-white' href="">
+                    <BsInstagram className='fs-4'/>
+                  </a>
+                  <a className='text-white' href="">
+                    <BsGithub className='fs-4'/>
+                  </a>
+                  <a className='text-white' href="">
+                    <BsYoutube className='fs-4'/>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className='text-white mb-4'>Información</h4>
+              <div className='footer-link d-flex flex-column'>
+                <Link className="text-white py-2 mb-1">Política de Privacidad</Link>
+                <Link className="text-white py-2 mb-1">Política de Reembolsos</Link>
+                <Link className="text-white py-2 mb-1">Política de Envíos</Link>
+                <Link className="text-white py-2 mb-1">Terminos y Condiciones</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className='text-white mb-4'>Cuenta</h4>
+              <div className='footer-link d-flex flex-column'>
+                <Link className="text-white py-2 mb-1">Busqueda</Link>
+                <Link className="text-white py-2 mb-1">Sobre Nosotros</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contacto</Link>
+                <Link className="text-white py-2 mb-1">Carrito</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className='text-white mb-4'>Links Directos</h4>
+              <div className='footer-link d-flex flex-column'>
+                <Link className="text-white py-2 mb-1">Accesorios</Link>
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Audifonos</Link>
+                <Link className="text-white py-2 mb-1">Smart Watches</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className='text-center mb-0 text-white'>&copy; { new Date().getFullYear()}; Powered by Students Suouls</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,137 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home_wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img src="images/ipad-banner2.jpg" className='img-fluid rounded-3' alt="main banner" />
+                <div className="main-banner-content position-absolute">
+                  <h4>La Mejor para los Profesionales</h4>
+                  <h5>iPad S13+ Pro</h5>
+                  <p>Desde $900.00 <br /> a $50.00/mes</p>
+                  <Link className='button'>Comprar Ahora</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img 
+                  src="images/catbanner-01.jpg" 
+                  className='img-fluid rounded-3' 
+                  alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Mas Vendidos</h4>
+                    <h5>Laptops</h5>
+                    <p>Desde $500.00 <br /> a $50.00/mes</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img 
+                  src="images/catbanner-03.jpg" 
+                  className='img-fluid rounded-3' 
+                  alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Novedades</h4>
+                    <h5>iPad Air</h5>
+                    <p>Desde $500.00 <br /> a $40.00/mes</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img 
+                  src="images/catbanner-02.jpg" 
+                  className='img-fluid rounded-3' 
+                  alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>15% de Descuento</h4>
+                    <h5>Smartwatch 4</h5>
+                    <p>Desde $100.00 <br /> a $20.00/mes</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img 
+                  src="images/catbanner-04.jpg" 
+                  className='img-fluid rounded-3' 
+                  alt="main banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Envío Gratis</h4>
+                    <h5>G33 Pro</h5>
+                    <p>Desde $200.00 <br /> a $50.00/mes</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service.png" alt="services" />
+                  <div>
+                    <h6>Envío Gratis</h6>
+                    <p className='mb-0'>Pedidos sobre $100</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-02.png" alt="services" />
+                  <div>
+                    <h6>Ofertas Diarias</h6>
+                    <p className='mb-0'>Desde 25%</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-03.png" alt="services" />
+                  <div>
+                    <h6>Sopporte 24/7</h6>
+                    <p className='mb-0'>Habla con expertos</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-04.png" alt="services" />
+                  <div>
+                    <h6>Precios Accesibles</h6>
+                    <p className='mb-0'>Precios de Fabrica</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-15'>
+                  <img src="images/service-05.png" alt="services" />
+                  <div>
+                    <h6>Pagos Seguros</h6>
+                    <p className='mb-0'>Pagos 100% Protegidos</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+            <div className="row">
+              <div className="col-12">
+                <div className="categories d-flex justify-content-between align-items-center">
+                  <div className='d-flex gap-30 align-items-center'>
+                    <div>
+                      <h6>Cámaras</h6>
+                      <p>10 Modelos</p>
+                    </div>
+                    <img src="images/camera.jpg" alt="camera" />
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
+    </>
   )
 }
 
